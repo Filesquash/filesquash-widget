ff38eaf715ead99ab79456a8a9bec3810f6567ce
Jean Lucas de Carvalho <jeanlucasdecarvalho@gmail.com>
2018-03-26 22:28:56 -0300

    Initial commit

---
+++ src/components/filesquash/filesquash.spec.ts
@@ -0,0 +1,18 @@
+import { render } from '@stencil/core/testing';
+import { FilesquashWidget } from './filesquash';
+
+describe('filesquash', () => {
+  it('should build', () => {
+    expect(new FilesquashWidget()).toBeTruthy();
+  });
+
+  describe('rendering', () => {
+    let element;
+    beforeEach(async () => {
+      element = await render({
+        components: [FilesquashWidget],
+        html: '<filesquash></filesquash>'
+      });
+    });
+  });
+});
